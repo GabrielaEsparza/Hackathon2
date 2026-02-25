be116aa8fda0b7adf4b9ef1ee21f9a00956f27e7
LuisGamez91 <luis.gamez91@hotmail.com>
2026-02-25 10:56:29 -0600

    Se agregó la clase Contacto

--- src/org/generation/hackathon/clases/Contacto.java
+++ src/org/generation/hackathon/clases/Contacto.java
@@ -1,5 +1,29 @@
 package org.generation.hackathon.clases;
 
 public class Contacto {
+	String name;
+	String telefono;
 
-}//Contacto
+	public Contacto(String name, String telefono) {
+		this.name = name;
+		this.telefono = telefono;
+	}// Constructor
+
+	public String getName() {
+		return name;
+	}// getName
+
+	public String getTelefono() {
+		return telefono;
+	}// getTelefono
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}// setTelefono
+
+	@Override
+	public String toString() {
+		return name.toUpperCase() + ": " + telefono;
+	}// toString
+
+}// Contacto
