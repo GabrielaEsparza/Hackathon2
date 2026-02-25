dc6166562e103ffc754d06090e252055517ef7d1
GabrielaEsparza1 <gerspaguetti1@gmail.com>
2026-02-25 13:21:22 -0600

    Cambiando las clase agenda a privado, el arreglo por un HashSet y los ultimos dos metodos acorde al HashSet

--- src/org/generation/hackathon/clases/Contacto.java
+++ src/org/generation/hackathon/clases/Contacto.java
@@ -1,29 +1,48 @@
 package org.generation.hackathon.clases;
 
+import java.util.Objects;
+
 public class Contacto {
-	String name;
-	String telefono;
 
-	public Contacto(String name, String telefono) {
-		this.name = name;
-		this.telefono = telefono;
-	}// Constructor
+    private String name;
+    private String telefono;
 
-	public String getName() {
-		return name;
-	}// getName
+    public Contacto(String name, String telefono) {
+        this.name = name;
+        this.telefono = telefono;
+    }// Constructor
 
-	public String getTelefono() {
-		return telefono;
-	}// getTelefono
+    public String getName() {
+        return name;
+    }// getName
 
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}// setTelefono
+    public String getTelefono() {
+        return telefono;
+    }// getTelefono
 
-	@Override
-	public String toString() {
-		return name.toUpperCase() + ": " + telefono;
-	}// toString
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }// setTelefono
 
-}// Contacto
+    @Override
+    public String toString() {
+        return name.toUpperCase() + ": " + telefono;
+    }// setTelefono
+
+	//-----------------para que HashMap no acepte elementos con el mismo nombre--------
+
+    // 
+    @Override
+   @Override
+public boolean equals(Object objeto) {
+    if (this == objeto) return true;
+    if (!(objeto instanceof Contacto)) return false;
+    Contacto contacto = (Contacto) objeto;
+    return name.equalsIgnoreCase(contacto.name);
+}
+
+    @Override
+    public int hashCode() {
+        return name.toLowerCase().hashCode();
+    }
+}//contacto
