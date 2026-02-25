32e750af1f849413617616e1623ddc4c21fc6a47
GabrielaEsparza1 <gerspaguetti1@gmail.com>
2026-02-25 11:17:07 -0600

    Creando lista de contactos

--- src/org/generation/hackathon/contactos/Agenda.java
+++ src/org/generation/hackathon/contactos/Agenda.java
@@ -1,5 +1,7 @@
 package org.generation.hackathon.contactos;
-
+import org.generation.hackathon.clases.Contacto;
 public class Agenda {
+	private Contacto[] contactos = new Contacto[10];
 
 }//Agenda
+
