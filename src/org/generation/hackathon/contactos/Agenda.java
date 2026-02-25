d138c85529e121fd0c6faab15e70cf3ab25673a0
GabrielaEsparza1 <gerspaguetti1@gmail.com>
2026-02-25 12:04:51 -0600

    Agrego metodo agenda llena y espacios libres

--- src/org/generation/hackathon/contactos/Agenda.java
+++ src/org/generation/hackathon/contactos/Agenda.java
@@ -3,5 +3,54 @@ import org.generation.hackathon.clases.Contacto;
 public class Agenda {
 	private Contacto[] contactos = new Contacto[10];
 
+	
+	
+	
+	
+	//----------------------------------Metodos----------------------------------------
+	
+	
+	//-------------------------------A;adir contacto----------------------------------
+	
+	
+	//-------------------------------Existe Contacto----------------------------------
+	
+	
+	//-------------------------------Listar Contactos----------------------------------
+	
+	
+	//-------------------------------Buscar contacto----------------------------------
+	
+	
+	
+	//-------------------------------Eliminar Contactos----------------------------------
+	
+	
+	
+	//-------------------------------Agenda Llena----------------------------------
+	public boolean agendaLlena() {
+	    for (Contacto c : contactos) {
+	        if (c == null) {
+	            return false; // esto si hay espacio libre
+	        }
+	    }
+	    return true; // esto si esta llena
+	}//agendaLlena
+	
+	//-------------------------------Espacios Libres----------------------------------
+	
+	public int espaciosLibres() {
+	    int contador = 0;
+
+	    for (Contacto c : contactos) {
+	        if (c == null) {
+	            contador++;
+	        }
+	    }
+
+	    return contador;
+	}//espaciosLibres
+	
 }//Agenda
+
 
